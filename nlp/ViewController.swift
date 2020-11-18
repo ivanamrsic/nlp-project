ea44ebc31c480e38cce598567eabcf2b4b96ac74
Ivana Mrsic <ivana.mrsic@fer.hr>
2020-11-18 17:34:06 +0000

    Initial commit

---
+++ nlp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nlp
+//
+//  Created by Ivana Mršić on 18.11.2020..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
