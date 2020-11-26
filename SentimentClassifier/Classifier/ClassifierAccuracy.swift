d110971dc49057edac26f52f288193aef711e491
Ivana Mrsic <ivana.mrsic@fer.hr>
2020-11-26 10:56:05 +0000

    Add MacOS app for generating classifier and testing it

---
+++ SentimentClassifier/Classifier/ClassifierAccuracy.swift
@@ -0,0 +1,39 @@
+//
+//  ClassifierAccuracy.swift
+//  SentimentClassifier
+//
+//  Created by Ivana Mršić on 25.11.2020.
+//
+
+import Foundation
+
+struct ClassifierAccuracy {
+    
+    // MARK: - Properties -
+    
+    let evaluation: Double
+    let training: Double
+    let validation: Double
+    
+    var dictionary: [String: String]? {
+        [
+            "evaluation": String(evaluation),
+            "training": String(training),
+            "validation": String(validation),
+        ]
+    }
+    
+    // MARK: - Initialization -
+    
+    init(evaluation: String?, training: String?, validation: String?) {
+        self.evaluation = Double(evaluation ?? "0") ?? 0
+        self.training = Double(training ?? "0") ?? 0
+        self.validation = Double(validation ?? "0") ?? 0
+    }
+    
+    init(evaluation: Double, training: Double, validation: Double) {
+        self.evaluation = evaluation
+        self.training = training
+        self.validation = validation
+    }
+}
---
+++ SentimentClassifier/Classifier/ClassifierUtil.swift
@@ -0,0 +1,80 @@
+//
+//  ClassifierUtil.swift
+//  SentimentClassifier
+//
+//  Created by Ivana Mršić on 25.11.2020.
+//
+
+import Foundation
+import CreateML
+
+class ClassifierUtil {
+    
+    // MARK: - Private properties -
+    
+    private static var data: MLDataTable {
+        try! MLDataTable(contentsOf: URL(fileURLWithPath: Constants.jsonFilePath))
+    }
+    
+    private static let (trainingData, testingData) =
+        data.randomSplit(by: Constants.splitProportion, seed: Constants.generatorSeed)
+    
+    private static var classifier: MLTextClassifier?
+    
+    private static var metadata = MLModelMetadata(
+        author: Constants.classifierAuthor,
+        shortDescription: Constants.classifierDescription,
+        version: Constants.classifierVersion
+    )
+}
+
+// MARK: - Public methods -
+
+extension ClassifierUtil {
+    
+    static func createClassifier() {
+        saveClassifierToMemory()
+    }
+}
+
+// MARK: - Private vars & methods -
+
+private extension ClassifierUtil {
+    
+    static var accuracy: ClassifierAccuracy? {
+        
+        classifier = try? MLTextClassifier(
+            trainingData: trainingData,
+            textColumn: Constants.textColumn,
+            labelColumn: Constants.labelColumn
+        )
+        
+        guard let classifier = classifier else { return nil }
+        
+        // metrics
+        let evaluationMetrics = classifier.evaluation(
+            on: testingData,
+            textColumn: Constants.textColumn,
+            labelColumn: Constants.labelColumn
+        )
+        
+        // accuracy
+        return ClassifierAccuracy(
+            evaluation: (1.0 - evaluationMetrics.classificationError) * 100,
+            training: (1.0 - classifier.trainingMetrics.classificationError) * 100,
+            validation: (1.0 - classifier.validationMetrics.classificationError) * 100
+        )
+    }
+    
+    static func saveClassifierToMemory() {
+                
+        FileManager.default.createFile(atPath: Constants.classifierFilePath, contents: nil)
+        
+        metadata.additional = accuracy?.dictionary
+        
+        try? classifier?.write(
+            to: URL(fileURLWithPath: Constants.classifierFilePath),
+            metadata: metadata
+        )
+    }
+}
